9068d911a8c44712b43aa96ba19e2ec4610abdcb
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-06-03 11:30:59 -0500

    Who needs tailwind anyways?

--- src/public/js/theme.js
+++ src/public/js/theme.js
@@ -1,13 +1,3 @@
-tailwind.config = {
-    theme: {
-      extend: {
-        colors: {
-          clifford: '#da373d',
-        }
-      }
-    }
-}
-
 const themeLink = document.createElement("link")
 themeLink.rel = "stylesheet"
 document.head.appendChild(themeLink)
